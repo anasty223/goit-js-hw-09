c42e6b59b864b7a3d36a32114bc6169464b8a16b
anasty223 <anasty22@gmail.com>
2021-11-29 19:56:56 +0200

    end

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,56 @@
 
+
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+const body = document.querySelector('body');
+
+   function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+      };
+      console.log(getRandomHexColor)
+
+startBtn.addEventListener('click', onStart);
+stopBtn.addEventListener('click', onStop);
+
+let intervalId = null;
+function onStart() {
+startBtn.setAttribute('disabled', true);
+     stopBtn.removeAttribute("disabled");
+
+     intervalId = setInterval(() => {
+ const color = `${getRandomHexColor()}`;
+       body.style.backgroundColor = color;
+  }, 1000);
+}
+function onStop() {
+    
+  startBtn.removeAttribute('disabled');
+   stopBtn.setAttribute("disabled", true);
+    clearInterval(intervalId);
+
+}
+
+// function onStart() {
+//     // if (onStart) {
+        
+//     //    startBtn.removeAttribute('disabled');
+//     //     return
+//     // } startBtn.setAttribute('disabled', true)
+
+// startBtn.addEventListener('click',  () => {
+//   intervalId = setInterval(() => {
+ 
+//   }, 1000);
+// });}
+// onStart()
+
+// function onStop(){
+// stopBtn.addEventListener('click', () => {
+//   clearInterval(intervalId);
+//   console.log(`Interval with id ${intervalId} has stopped!`);
+// });
+// }
+// onStop()
+
+
+
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,81 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
-}
+import Notiflix from 'notiflix';
+
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', onSubmit);
+
+function onSubmit(evt) {
+  evt.preventDefault();
+  
+  const formData = new FormData(form);
+  console.log(formData);
+  const objectData = {};//это обьект наших записей в полях
+
+    formData.forEach((value, name) => {//тут мы определяем что в полях стоит и это направляем в нейм и велью
+    objectData[name] = Number(value);//говорим что велью это цыфра
+    });
+  let amount = objectData.amount;
+  console.log('amount', amount);
+
+  let delay = Number(objectData.delay);
+  console.log("delay",delay)
+
+
+ let step = Number(objectData.step);
+ console.log('step', step)
+
+  let position = '';//єто номер создаваемого промиза
+ for (let index = 0; index <= amount; index++) {
+  position = index; 
+
+   delay += step;//к-во времени мы плюсуем шаги, переходим
+
+ ;
+  
+  const createPromise = (position, delay) => {
+  
+    return new Promise((resolve, reject) => {
+      const shouldResolve = Math.random() > 0.3;
+     
+    setInterval(() => {
+      
+      
+        if (shouldResolve) {
+      // Fulfill 
+      resolve({ position, delay });
+    } 
+      // Reject
+      reject({ position, delay });
+    
+       }, delay)
+    });
+   
+  };
+   
+ createPromise(position, delay)
+    .then(({ position, delay }) => {
+    Notiflix.Notify.success(` Fulfilled promise ${position} in ${delay}ms`);
+  })
+  .catch(({ position, delay }) => {
+   Notiflix.Notify.failure(` Rejected promise ${position} in ${delay}ms`);
+  }); 
+
+};
+
+ }
+ 
+   
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+//написать скрипт которій при submit вызывает функцию
+//createPromise(position, delay) столько раз сколько ввели amount
+//При каждом вызове передай ей номер создаваемого промиса (position) 
+//и задержку учитывая введенную пользователем первую задержку
+//(delay) и шаг(step).
+
+
+// Дополни код функции createPromise так, чтобы она возвращала один промис,
+//   который выполянется или отклоняется через delay времени.
+//   Значением промиса должен быть объект, в котором будут свойства position и 
+//   delay со значениями одноименных параметров.Используй начальный код функции для выбора того,
+//   что нужно сделать с промисом - выполнить или отклонить.
